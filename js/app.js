7d219e00ee379d9df8da73438b5eb6c5f144b672
alkhateeb49 <mohamadalkhateeb49@yahoo.com>
2020-12-28 17:21:38 +0200

    lab02

---
+++ js/app.js
@@ -0,0 +1,52 @@
+'use strict';
+var Name=prompt("What's your name?").toLowerCase();
+alert("Welcome "+Name);
+
+
+var q1=prompt("Is my favorite color black? y/n");
+if(q1.toLowerCase() === "y"){
+//   console.log("You are right!");
+  alert("You are right!");
+}else{
+//   console.log("Not correct :(");
+  alert("Not correct :(");
+}
+
+var q2=prompt("Is it my favorite programming language is C++? y/n");
+if(q2.toLowerCase() === "n"){
+//   console.log("You are right!");
+  alert("You are right!");
+}else{
+//   console.log("Not correct :(");
+  alert("Not correct :(");
+}
+
+var q3=prompt("Am I 22 years old? y/n");
+if(q3.toLowerCase() === "y"){
+//   console.log("You are right!");
+  alert("You are right!");
+}else{
+//   console.log("Not correct :(");
+  alert("Not correct :(");
+}
+
+var q4=prompt("Is my mother tongue Arabic? y/n");
+if(q4.toLowerCase() === "y"){
+//   console.log("You are right!");
+  alert("You are right!");
+}else{
+//   console.log("Not correct :(");
+  alert("Not correct :(");
+}
+
+var q5=prompt("Do I love all pets? y/n");
+if(q5.toLowerCase() === "y"){
+//   console.log("You are right!");
+  alert("You are right!");
+}else{
+//   console.log("Not correct :(");
+  alert("Not correct :(");
+}
+
+alert("Thank you "+Name);
+
